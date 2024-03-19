4457d21a15e72c2bcdd6b91d017a5d053bfe7e8a
MrGutsy45 <107184245+MrGutsy45@users.noreply.github.com>
2024-03-19 13:53:33 +0100

    Update poc.py
    ouais ouais ouais

--- poc.py
+++ poc.py
@@ -1,28 +1,29 @@
 import mysql.connector 
-
+from pyope.ope import OPE
 ##Implémentation ORE
 
 
 
 ##Coté client
 ## Insérer un nouveau salaire coté client
-def client_put(id,val,connec):
+def client_put(id,val,connec,cipher):
     
     ## Implementation de l'Order Reavealing Encryption
-    valc = generer_cle(b'long key' * 2)
-    
+    valc = cipher.encrypt(val)
     server_put(id,valc,connec)
+    
 ## Comparer deux salaires entre eux
 def client_compare(id1,id2,connec):
     ida = server_compare(id1,id2)
     return ida
 
 ## Additionner la somme des salaires
-def client_sum(connec):
+def client_sum(connec, cipher):
     encrypted_sum = server_sum(connec)
     ## Coté dechiffré client et affichage
-
-
+    valdc = cipher.decrypt(encrypted_sum)
+    return valdc
+    
 ##Coté serveur
 ## Insérer un nouveau salaire coté serveur
 def server_put(id,valc,connec):
@@ -34,10 +35,21 @@ def server_put(id,valc,connec):
     val = (id, valc)
     cur.execute(sql,val)
     connec.commit()
+    
 def server_compare(id1,id2,connec):
     ## Comparaison avec ORE et retour du resultat
-    
-    pass
+    cur = connec.cursor()
+    query = "SELECT salaire FROM salaire_employe WHERE nom = %s"
+    cur.execute(query, (id1,))
+    val1 = curseur.fetchone()
+    cur.execute(query, (id2,))
+    val1 = curseur.fetchone()
+    if val1 == val2 :
+        return id1 + " a le même salaire que " + id2
+    elif val1 <= val2:
+        return id2 + " a le plus grand salaire"
+    else:
+        return id1 + " a le plus grand salaire"
 
 def server_sum(connec):
     cur = connec.cursor()
@@ -52,6 +64,10 @@ if __name__ == '__main__':
     db_host = 'localhost'
     db_db = 'poc_question31'
     server_host = 'server'
+
+    cle = "*v0jTYiU$aLydY3!P/OcEC7A&97P6+&XH#UIHvHQptFt4$*j5t!cRt3N74tYJDte"
+    cipher = OPE(cle)
+    
     #Fonction de connexion à la base de données
     def db_connexion(user,pas,host,db):
         cnx = mysql.connector.connect(user=user, password=pas, host=host,
@@ -68,13 +84,13 @@ if __name__ == '__main__':
     if choice == "1":
         nom = input("Entrez l'id de l'employé : ")
         salaire = input("Entrez le salaire de l'employé : ")
-        client_put(nom,salaire,connec)
+        client_put(nom,salaire,connec,cipher)
         print("Salaire inséré avec succès")
     if choice == "2":
         nom1 = input("Entrez le premier id : ")
         nom2 = input("Entrez le deuxième id : ")
-        print(f'le salaire le plus important est : {client_compare(nom1,nom2,connec)}')
+        print(f'{client_compare(nom1,nom2,connec)}')
     if choice == "3":
-        print(client_sum(connec))
+        print(client_sum(connec, cipher))
     if choice == "4":
         exit()
